6c2285cdc329fec7e436190afae863065645a73f
Pierre Wieser <p.wieser@trychlos.org>
2024-06-10 14:45:53 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:orderable-stack',
-    version: '1.0.2',
+    version: '1.0.3-rc',
     summary: 'An IOrderableStack interface and its OrderableStack implementation',
     git: 'https://github.com/trychlos/pwix-orderable-stack.git',
     documentation: 'README.md'
